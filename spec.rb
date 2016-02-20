4c90fa9c0a6c90b88d5b43b6f44f12f2d6fb9a66
Rushan <trlearning@mail.ru>
2016-02-21 02:49:15 +0400

    [add] new expectations

--- spec.rb
+++ spec.rb
@@ -12,4 +12,30 @@ RSpec.describe Script do
 		s1 << " and stuff!"
 		expect(s1).to match(/.*things.*stuff.*/)
 	end
+
+	describe 'just for learning purposes' do
+		describe 'types/classes' do
+			before(:each) do
+				@inst1 = Script.new(150)
+			end
+
+			it '@inst1 instance of Script' do
+				expect(@inst1).to be_an_instance_of(Script)
+				expect(@inst1).to be_a(Script)
+			end
+
+			it '@inst1 instance of Script (aliases for be_a)' do
+				expect(@inst1).to be_an(Script)
+				expect(@inst1).to be_a_kind_of(Script)
+			end
+		end
+
+		describe 'truthiness' do
+			it 'some of examples' do
+				expect(true).to be_truthy
+				expect(true).to be true
+				expect(nil).to be_falsy
+			end
+		end
+	end
 end
