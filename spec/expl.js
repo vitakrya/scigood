db4c1fc87b1b1d6d935bcd6fec7ddf585defbc8c
Rushan <trlearning@mail.ru>
2016-02-24 03:30:58 +0400

    experience with asynchronous specs

--- spec/expl.js
+++ spec/expl.js
@@ -3,3 +3,39 @@ describe('Expl', function(){
 		expect(6).toBe(6);
 	});
 });
+
+describe('Asynchronous specs', function() {
+  var value;
+
+  beforeEach(function(done) {
+  	setTimeout(function() {
+  		value = 0;
+  		done();
+  	}, 1);
+  });
+
+  it('should support async execution of test preparation and expectations', function(done) {
+  	value++;
+  	expect(value).toBeGreaterThan(0);
+  	done();
+  });
+
+  describe('long asynchronous specs', function() {
+  	var originalTimeout;
+
+  	beforeEach(function() {
+  		originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+  		jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+  	});
+
+  	it('takes a long time', function(done) {
+  		setTimeout(function() {
+  			done();
+  		}, 9000);
+  	});
+
+  	afterEach(function() {
+  		jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  	});
+  });
+});
