5ae29fe01526703bc5dba540da53038bcfc16809
Rushan <trlearning@mail.ru>
2016-02-24 04:50:29 +0400

    add spec

--- spec/someSpecs.js
+++ spec/someSpecs.js
@@ -2,4 +2,8 @@ describe('spec1', function() {
 	it('5 should not be 15', function() {
 		expect(5).not.toBe(15);
 	});
+
+	it('5 should not be 26', function() {
+		expect(5).not.toBe(26);
+	});
 });
