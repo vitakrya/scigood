87447efec1a46d50ba764f40ab3fda977ef75f7c
Rushan <trlearning@mail.ru>
2016-02-24 04:54:36 +0400

    [modified] spec

--- spec/someSpecs.js
+++ spec/someSpecs.js
@@ -3,7 +3,7 @@ describe('spec1', function() {
 		expect(5).not.toBe(15);
 	});
 
-	it('5 should not be 26', function() {
-		expect(5).not.toBe(26);
+	it('5 should not be 20', function() {
+		expect(5).not.toBe(20);
 	});
 });
