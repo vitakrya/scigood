28e7218c95afa50de4285e88ba7b5c0c0dbcf062
Rushan <vinder_2@mail.ru>
2016-12-03 20:49:50 +0300

    Change spec

--- script_spec.rb
+++ script_spec.rb
@@ -4,8 +4,8 @@ Coveralls.wear!
 
 RSpec.describe Script do
   it 'correctly multiplies to twice' do
-    inst1 = Script.new(90)
-    expect(inst1.twice).to eq(180)
+    inst1 = Script.new(9)
+    expect(inst1.twice).to eq(18)
   end
 
   it "string check" do
